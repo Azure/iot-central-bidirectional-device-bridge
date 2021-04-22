94f935fa3abc98bb2d860a131e021027b8e68db0
Pericles Alves <peolivei@microsoft.com>
2021-04-22 13:34:45 -0700

    Refactoring SubscriptionService

--- DeviceBridge/Services/IDataSubscriptionService.cs
+++ DeviceBridge/Services/IDataSubscriptionService.cs
@@ -7,22 +7,12 @@ using NLog;
 
 namespace DeviceBridge.Services
 {
-    public interface ISubscriptionService
+    public interface IDataSubscriptionService
     {
-        Task<DeviceSubscription> CreateOrUpdateConnectionStatusSubscription(Logger logger, string deviceId, string callbackUrl, CancellationToken cancellationToken);
-
         Task<DeviceSubscriptionWithStatus> CreateOrUpdateDataSubscription(Logger logger, string deviceId, DeviceSubscriptionType subscriptionType, string callbackUrl, CancellationToken cancellationToken);
-
-        Task DeleteConnectionStatusSubscription(Logger logger, string deviceId, CancellationToken cancellationToken);
 
         Task DeleteDataSubscription(Logger logger, string deviceId, DeviceSubscriptionType subscriptionType, CancellationToken cancellationToken);
 
-        Task<DeviceSubscription> GetConnectionStatusSubscription(Logger logger, string deviceId, CancellationToken cancellationToken);
-
         Task<DeviceSubscriptionWithStatus> GetDataSubscription(Logger logger, string deviceId, DeviceSubscriptionType subscriptionType, CancellationToken cancellationToken);
-
-        Task StartDataSubscriptionsInitializationAsync();
-
-        Task SynchronizeDeviceDbAndEngineDataSubscriptionsAsync(string deviceId, bool useInitializationList = false);
     }
 }
