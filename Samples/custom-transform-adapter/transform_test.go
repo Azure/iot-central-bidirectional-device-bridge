7b750421ea55d3f249f923e29999909b29065849
Pericles Alves <peolivei@microsoft.com>
2021-04-30 17:01:21 -0700

    Initial version

---
+++ Samples/custom-transform-adapter/transform_test.go
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTransformEngineAddTransform(t *testing.T) {
+	engine := NewTransformEngine()
+	assert.NoError(t, engine.AddTransform("pass-through", "."))
+	assert.NoError(t, engine.AddTransform("sample", ". | {a, b}"))
+	assert.Error(t, engine.AddTransform("invalid", ".{a, b}"))
+}
+
+// SImple query that maps { "a": 1 } to { "b": 1}.
+func ExampleTransformEngineExecuteSample() {
+	engine := NewTransformEngine()
+	engine.AddTransform("sample", "{ b: .a}")
+	result, _ := engine.Execute("sample", map[string]interface{}{"a": 1})
+	fmt.Println(result)
+	// Output: map[b:1]
+}
+
+func TestTransformEngineExecuteNotFound(t *testing.T) {
+	engine := NewTransformEngine()
+	assert.NoError(t, engine.AddTransform("a-transform", "."))
+	_, err := engine.Execute("another-transform", map[string]interface{}{})
+	assert.EqualError(t, err, "Transformation for id another-transform not found")
+}
+
+func TestTransformEngineExecuteFail(t *testing.T) {
+	engine := NewTransformEngine()
+	assert.NoError(t, engine.AddTransform("bad-transform", "{(.a): 1}"))
+	_, err := engine.Execute("bad-transform", map[string]interface{}{"a": 1})
+	assert.EqualError(t, err, "Transform id bad-transform failed: expected a string for object key but got: number (1)")
+}
+
+func TestTransformEngineExecuteMultipleResults(t *testing.T) {
+	engine := NewTransformEngine()
+	assert.NoError(t, engine.AddTransform("multiple-results", "{a: 1},{b: 2}"))
+	_, err := engine.Execute("multiple-results", map[string]interface{}{})
+	assert.EqualError(t, err, "Transform id multiple-results generated multiple results")
+}
